e541eb8069b4566800d6ef95bfe0d42c2230b2c0
SallGood <32182117+sallgoood@users.noreply.github.com>
2023-05-22 11:34:18 +0900

    initialize the project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "me.myoungsokang"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
